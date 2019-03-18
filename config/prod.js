0121c8dde5455a9df547f1c449c2d6eabf9da723
Anish Mathew <anishk83@gmail.com>
2019-03-17 20:27:24 -0500

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE.CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE.CLIENT_SECRET,
+  movieDBApiKey: process.env.MOVIE_DB_API_KEY,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
--- index.js
+++ index.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // console.log that your server is up and running
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
