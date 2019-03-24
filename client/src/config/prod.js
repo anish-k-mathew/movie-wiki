7f846acc6c6e25741f424f10bd119afdfe11e590
Anish Mathew <anishk83@gmail.com>
2019-03-24 18:50:24 -0500

    updated ficx

---
+++ client/src/config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  movieDBApiKey: process.env.MOVIE_DB_API_KEY,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
