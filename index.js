98e028aeaf946092e3d2adcf3617708edead2d07
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-10 16:00:09 -0500

    add conn string db

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 const db = knex({
   client: 'pg',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
   }
 })
