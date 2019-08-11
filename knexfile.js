cc3655fcc0fb7ce9ab03a4cb5d728791707448fe
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-10 19:33:51 -0500

    knewx

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+ 
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/movie-wiki-db',
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DATABASE_URL,
+      ssl: true
+    }
+  }
+
+};
