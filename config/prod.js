cdbd19818bc8747037f901d3ba806fd30bb87ffa
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-16 16:37:31 -0500

    changed connection db detail

--- config/prod.js
+++ config/prod.js
@@ -2,9 +2,9 @@ module.exports = {
   movieDBApiKey: process.env.MOVIE_DB_API_KEY,
 
   dbConnection: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
-      host: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL
     },
     pool: { min: 0, max: 7 }
   }
