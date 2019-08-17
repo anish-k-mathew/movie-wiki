abb7aaae82333ed85f6ae24af675bf579451e522
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-16 20:33:04 -0500

    updated node & npm versions

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      connectionString: "postgres://zpkyjhfhcmttnb:e07eecab726dc0a01bd856167a45aafd019c722378b26b9514c429d109ea0438@ec2-174-129-227-205.compute-1.amazonaws.com:5432/d7ipmuc10o4eic",
+      connectionString: process.env.DATABASE_URL,
       ssl: true
     }
   }
