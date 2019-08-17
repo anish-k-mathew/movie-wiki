9bd21220e00da8896669810191872e27e64debcd
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-17 18:41:21 -0500

    fixed sendfile issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const knex = require("knex");
-
+const path = require("path");
 
 const config = require("./config/keys");
 const db = knex(config.dbConnection);
@@ -80,16 +80,15 @@ app.get("/watchlist", (req, res) => {
 });
 
 // Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
- 
 const PORT = process.env.PORT || 5070;
 // console.log that your server is up and running
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
