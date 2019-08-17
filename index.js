4e1027fab778f14f6698bf4ef1da80bbab1c63f4
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-17 10:59:47 -0500

    fixed sendfile issue

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ app.use(function(req, res, next) {
 });
 
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 //if (process.env.NODE_ENV === "production") {
   // Express will service prod assets like main.js file
@@ -98,6 +97,8 @@ app.get("/watchlist", (req, res) => {
       return res.json(response);
     });
 
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
