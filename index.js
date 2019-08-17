52bb7316548e11199ad0dd1d9d6872c9f265a24e
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-17 10:35:08 -0500

    fixed sendfile issue

--- index.js
+++ index.js
@@ -24,16 +24,20 @@ app.use(function(req, res, next) {
   next();
 });
 
-if (process.env.NODE_ENV === "production") {
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+//if (process.env.NODE_ENV === "production") {
   // Express will service prod assets like main.js file
-  app.use(express.static("client/build"));
+ // app.use(express.static("client/build"));
 
   // Express will serve index.html if it doesn't recognize the route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+ // const path = require("path");
+  //app.get("*", (req, res) => {
+   // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  //});
+//
 
 app.post("/register", (req, res) => {
   db("user_profile").insert({
