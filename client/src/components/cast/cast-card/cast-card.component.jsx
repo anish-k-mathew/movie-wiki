24f825f3006e1ee6f5109c9173e0334e1a521783
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-23 15:01:20 -0500

    adjust cast list rendering

--- client/src/components/cast/cast-card/cast-card.component.jsx
+++ client/src/components/cast/cast-card/cast-card.component.jsx
@@ -5,7 +5,7 @@ class CastCard extends Component {
   render() {
     const { id, profile_path } = this.props.filteredItem;
     return (
-      <div className='col p-2 text-warning' key={id}>
+      <div className='col-lg-2 p-3 text-warning' key={id}>
         <Link to={`persons/${id}`}>
           <img
             src={`https://image.tmdb.org/t/p/original${profile_path}`}
