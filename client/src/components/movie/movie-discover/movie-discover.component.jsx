363227559ec54ecf0a80999922055abb9b2ebcf2
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-25 14:38:53 -0500

    added more years and another language

--- client/src/components/movie/movie-discover/movie-discover.component.jsx
+++ client/src/components/movie/movie-discover/movie-discover.component.jsx
@@ -68,6 +68,33 @@ class MovieDiscover extends React.Component {
             <option value='2012'>2012</option>
             <option value='2011'>2011</option>
             <option value='2010'>2010</option>
+            <option value='2009'>2009</option>
+            <option value='2008'>2008</option>
+            <option value='2007'>2007</option>
+            <option value='2006'>2006</option>
+            <option value='2005'>2005</option>
+            <option value='2004'>2004</option>
+            <option value='2003'>2003</option>
+            <option value='2002'>2002</option>
+            <option value='2001'>2001</option>
+            <option value='2000'>2000</option>
+            <option value='1999'>1999</option>
+            <option value='1998'>1998</option>
+            <option value='1997'>1997</option>
+            <option value='1996'>1996</option>
+            <option value='1995'>1995</option>
+            <option value='1994'>1994</option>
+            <option value='1993'>1993</option>
+            <option value='1992'>1992</option>
+            <option value='1991'>1991</option>
+            <option value='1990'>1990</option>
+            <option value='1989'>1989</option>
+            <option value='1988'>1988</option>
+            <option value='1987'>1987</option>
+            <option value='1986'>1986</option>
+            <option value='1985'>1985</option>
+            <option value='1984'>1984</option>
+            <option value='1983'>1983</option>
           </select>
           <label className='text-white'>Language</label>
           <select
@@ -78,7 +105,8 @@ class MovieDiscover extends React.Component {
             <option value='en'>English</option>
             <option value='hi'>Hindi</option>
             <option value='ml'>Malayalam</option>
-            <option value='tm'>Tamil</option>
+            <option value='ta'>Tamil</option>
+            <option value='te'>Telugu</option>
           </select>
           <input type='submit' value='Submit' />
         </form>
