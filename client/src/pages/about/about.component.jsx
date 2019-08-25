8f48cb47502ce0a126b44889b9f5b91f2c9d57ec
Mathew, Anish K <anish.k.mathew@accenture.com>
2019-08-25 02:24:36 -0500

    added about page nfo;

--- client/src/pages/about/about.component.jsx
+++ client/src/pages/about/about.component.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './about.styles.scss';
 const About = () => (
-  <div>
-    <div>Hello</div>
-  </div>
+  <React.Fragment>
+    <div className='about-section-heading'>
+      <div>Anish Mathew</div>
+      <ul>
+        <li>email - mathew.anishk@gmail.com</li>
+        <li>
+          Portfolio Site -
+          <a href='http://www.anishkmathew.com'> www.anishkmathew.com</a>
+        </li>
+      </ul>
+    </div>
+    <hr />
+    <div className='about-section-heading-tech'>
+      <div>Tech Stack</div>
+      <ul>
+        <li>UI - React.js</li>
+        <li>Backend - Node.js</li>
+        <li>DB - Postgres</li>
+        <li>Authentication - Firebase</li>
+        <li>Environment - Heroku</li>
+        <li>API Support - TheMovieDB</li>
+      </ul>
+    </div>
+  </React.Fragment>
 );
 
 export default About;
